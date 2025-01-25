6f80b48862b8d76c381ef1c63d45f9850f79eae0
Basakeee <127082177+Basakeee@users.noreply.github.com>
2025-01-26 01:43:22 +0700

    Reapply "Merge branch 'Chote' into Design"
    This reverts commit 5f722738d57059cbedfa5ee9fa3a91f9cfd06990.

---
+++ Assets/Chote/Scripts/VideoController.cs
@@ -0,0 +1,56 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class VideoController : MonoBehaviour
+{
+    public static VideoController instance;
+    public GameObject videoPlayer;
+    public GameObject screen;
+
+    [Header("Input video")]
+    public List<VideoClip> videoList;
+
+
+    VideoPlayer vd;
+    RawImage rawImage;
+
+    private void Awake()
+    {
+        if (instance) Debug.LogWarning("There is more than 1 instance of VideoController in the scene! REMOVE THE EXTRA");
+        instance = this;
+    }
+    void Start()
+    {
+        vd = videoPlayer.GetComponent<VideoPlayer>();
+        rawImage = screen.GetComponent<RawImage>();
+
+        vd.loopPointReached += OnVideoEnd;
+    }
+    private void OnVideoEnd(VideoPlayer vp)
+    {
+        Debug.Log("Video finished playing!");
+        TurnOffVideo();
+    }
+
+    public void TurnOffVideo()
+    {
+        videoPlayer.SetActive(false);
+        screen.SetActive(false);
+    }
+
+    public void TurnOnVideo()
+    {
+        videoPlayer.SetActive(true);
+        screen.SetActive(true);
+    }
+
+    public void PlayVideo(int index)
+    {
+        TurnOffVideo();
+        vd.clip = videoList[index];
+        TurnOnVideo();
+    }
+}
